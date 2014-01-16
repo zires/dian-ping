3eda4e8f47b5a29c5840792840e5dd280141a361
zires <zshuaibin@gmail.com>
2014-01-16 15:05:43 +0800

    v0.0.7

--- lib/dian_ping.rb
+++ lib/dian_ping.rb
@@ -6,7 +6,7 @@ require 'dianping/parameter'
 class DianPing
   include HTTParty
 
-  VERSION = '0.0.6'
+  VERSION = '0.0.7'
 
   attr_accessor :key, :secret
 
--- lib/dianping/parameter.rb
+++ lib/dianping/parameter.rb
@@ -22,24 +22,46 @@ class DianPing
       client.get(route, self)
     end
 
+    # 获取指定商户信息
     def business(id)
       add :business_id, id
       get '/v1/business/get_single_business'
     end
 
+    # 获取指定商户最新点评片断
     def reviews(id)
       add :business_id, id
       get '/v1/review/get_recent_reviews'
     end
 
+    # 获取支持商户搜索的最新分类列表
     def categories(city = nil)
       add :city, city
       get '/v1/metadata/get_categories_with_businesses'
     end
 
+    # 获取支持商户搜索的最新城市下属区域列表
     def regions(city = nil)
       add :city, city
       get '/v1/metadata/get_regions_with_businesses'
+    end
+
+    # 获取支持在线预订的全部商户ID列表
+    def reservations(city)
+      add :city, city
+      get '/v1/reservation/get_all_id_list'
+    end
+
+    # 获取当前在线的全部团购ID列表
+    def deals(city)
+      add :city, city
+      get '/v1/deal/get_all_id_list'
+    end
+
+    # 获取指定团购信息
+    def deal(deal_id)
+      add :deal_id, deal_id
+      get '/v1/deal/get_single_deal'
     end
 
   end
